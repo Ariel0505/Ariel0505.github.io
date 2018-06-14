771e514931cd0d59a62f6b9c2130ff14559c38ad
Ariel0505 <36212383+Ariel0505@users.noreply.github.com>
2018-06-14 10:45:46 -0700

    Update and rename wordguesser.html to everything.js

---
+++ everything.js
@@ -0,0 +1,59 @@
+function tabulateAnswers() {
+
+    var c1score = 0;
+    var c2score = 0;
+    var c3score = 0;
+    var c4score = 0;
+
+
+    var choices = document.getElementsByTagName('input')
+
+    for (i=0; i<choices.length; i++) {
+        // if the radio is checked..
+        if (choices[i].checked) {
+            // add 1 to that choice's score
+            if (choices[i].value === 'c1') {
+                c1score = c1score + 1;
+            }
+            if (choices[i].value === 'c2') {
+                c2score = c2score + 1;
+            }
+            if (choices[i].value === 'c3') {
+                c3score = c3score + 1;
+            }
+            if (choices[i].value === 'c4') {
+                c4score = c4score + 1;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+    var maxscore = Math.max(c1score,c2score,c3score,c4score);
+
+
+    var answerbox = document.getElementById('answer');
+    if (c1score === maxscore) {
+        answerbox.innerHTML = "You are Cady";
+    }
+    if (c2score === maxscore) {
+        answerbox.innerHTML = "You are Gretchen!";
+    }
+    if (c3score === maxscore) {
+        answerbox.innerHTML = "You are Karen.";
+    }
+    if (c4score === maxscore) {
+        answerbox.innerHTML = "You are Regina!";
+
+    }
+    console.log("hello");
+}
+function resetAnswer() {
+    var answerbox = document.getElementById('answer');
+    answerbox.innerHTML = "Your result will show up here!";
+}
